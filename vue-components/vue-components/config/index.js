609dd65aa6d73f681134c46d6db6eb841c252a09
邹杰 <787352714@qq.com>
2019-09-11 17:38:46 +0800

    修改

--- vue-components/vue-components/config/index.js
+++ vue-components/vue-components/config/index.js
@@ -10,7 +10,15 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api': {
+        target: '',//后端接口地址
+        changeOrigin: true,//是否允许跨越
+        pathRewrite: {
+            '^/api': '/api',//重写,
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
---
+++ vue-components/vue-components/src/components/issueTest/Issues.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-table :data="issueData" style="width: 100%">
+    <el-table-column v-for="col in columns"
+      :prop="col.id"
+      :key="col.id"
+      :label="col.label"
+      :width="col.width">
+    </el-table-column>
+  </el-table>
+</template>
+<script>
+import { getIssues } from '~api'
+export default {
+  data(){
+    return {
+      columns:[],
+      issueData:[],
+    }
+  },
+  mounted(){
+    this.setIssueList();
+  },
+  methods:{
+    async setIssueList(){
+      try {
+        let data= await getIssues();
+        console.log('data :', data);
+      } catch (error) {
+        console.log('error :', error);
+      }
+    }
+  }
+}
+</script>
