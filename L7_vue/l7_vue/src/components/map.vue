508d6d62a0e3ce2a372722a51f404067a3ebcd83
pedeg <jiezou714@163.com>
2019-09-26 00:08:30 +0800

    添加L_vue坑

---
+++ L7_vue/l7_vue/src/components/map.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="L7-map">
+    <div :id='id'></div>
+  </div>
+</template>
+<script>
+import * as L7 from '@antv/l7';
+export default {
+  data() {
+    const scene = new L7.scene({
+      id:this.id,
+      mapStyle:this.mapStyle,
+      center:this.center,
+      pitch:this.pitch
+    })
+  },
+  props:{
+    id:{
+      type: String,
+      default: 'map'
+    },
+    center:{
+      type: Array,
+      default: ()=>{
+        return []
+      }
+    },
+    viewMode:{
+      type: String,
+      default: '3D'
+    },
+    zoom:{
+      type: Number,
+      default: 0
+    },
+    mapStyle:{
+      type: String,
+      default: 'dark'
+    }
+  }
+}
+</script>
