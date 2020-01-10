b6d9b7be39f0b24fe9b5cd45e94fbd749ea59828
pedeg <jiezou714@163.com>
2020-01-11 01:12:30 +0800

    vant

---
+++ vue.js/vue-vant/src/views/About.vue
@@ -0,0 +1,67 @@
+<template>
+  <van-address-edit
+    :area-list="areaList"
+    show-postal
+    show-delete
+    show-set-default
+    show-search-result
+    :search-result="searchResult"
+    :area-columns-placeholder="['请选择', '请选择', '请选择']"
+    @save="onSave"
+    @delete="onDelete"
+    @change-detail="onChangeDetail"
+  />
+</template>
+<script>
+import { Toast } from 'vant';
+export default {
+  data() {
+    return {
+      areaList:{
+        province_list: {
+          110000: '北京市',
+          120000: '天津市'
+        },
+        city_list: {
+          110100: '北京市',
+          110200: '县',
+          120100: '天津市',
+          120200: '县'
+        },
+        county_list: {
+          110101: '东城区',
+          110102: '西城区',
+          110105: '朝阳区',
+          110106: '丰台区',
+          120101: '和平区',
+          120102: '河东区',
+          120103: '河西区',
+          120104: '南开区',
+          120105: '河北区',
+          // ....
+        }
+      },
+      searchResult: []
+    }
+  },
+
+  methods: {
+    onSave() {
+      Toast('save');
+    },
+    onDelete() {
+      Toast('delete');
+    },
+    onChangeDetail(val) {
+      if (val) {
+        this.searchResult = [{
+          name: '黄龙万科中心',
+          address: '杭州市西湖区'
+        }];
+      } else {
+        this.searchResult = [];
+      }
+    }
+  }
+}
+</script>
---
+++ vue.js/vue-vant/src/views/Home.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="search">
+    <div id="searchbar">
+      <form action="/">
+        <van-search
+          v-model="searchValue"
+          placeholder="请输入搜索关键词"
+          show-action
+          @input="onSearch"
+          @cancel="onCancel"
+          @blur="onCancel"
+        />
+      </form>
+    </div>
+    <div id="searchContainer">
+      <van-popup v-model="show" :overlay="false" :get-container="getContainer" :style="{ width: '100%',position:'absolute',top:'33%','text-align':'left'}">
+        <van-list>
+          <van-cell
+            v-for="item in list"
+            :key="item"
+            :title="item"
+          />
+        </van-list>
+      </van-popup>
+    </div>
+    <div class="goodsButton">
+      <!-- 利用循环v-for -->
+      <van-button v-for="(item,index) in goodsList" :key="index" plain hairline type="default" size="mini" :style="{'margin-bottom':'10px','margin-left':'10px' }">{{item}}</van-button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+      searchValue:'',
+      show: false,
+      list:[],
+      goodsList:['货物1','货物2','货物3','货物221','货物111','货物123123','货物11','货物1','货物1']
+    }
+  },
+  methods: {
+    onSearch(){
+      //如有接口就在此获取数据赋值给this.list，注意，每输入一次都会调用
+      this.show= true;
+      this.list=[this.searchValue,this.searchValue,this.searchValue,this.searchValue]
+    },
+    onCancel(){
+      this.searchValue="";
+      this.list=[];
+      this.show=false;
+    },
+    getContainer(){
+      return document.querySelector('.searchbar');
+    }
+  },
+}
+</script>
+<style scoped>
+  .search{
+    width: 100%;
+    height: 80%;
+  }
+  .goodsButton{
+    padding-right: 10px;
+    margin-top: 15px;
+  }
+</style>
